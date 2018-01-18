1cc0d3e81d5eadf4b7520851fe1337270edba064
Craig J. Bass <craig@madetech.co.uk>
2018-01-18 15:59:56 +0000

    Refactor duplication from conditionals

--- lib/santa_maria/gateway/santa_maria_v2.rb
+++ lib/santa_maria/gateway/santa_maria_v2.rb
@@ -11,16 +11,8 @@ module SantaMaria
         result = JSON.parse(response.body)
 
 
-        result['products'].each do |p|
-          product = SantaMaria::Domain::Product.new(self)
-          product.global_id = p['globalId']
-          product.type = p['productType']
-          product.name = p['name']
-          product.uri_name = p['uri']
-          product.description = p['localSlogan']
-          product.image_url = p.dig('packshots', 0, 'm')
-
-          yield product
+        result['products'].each do |product|
+          yield new_product(product)
         end
       end
 
@@ -33,31 +25,13 @@ module SantaMaria
 
         product['sku'].each do |sku|
           if sku['colorIds'].nil?
-            variant = SantaMaria::Domain::Variant.new
-            variant.article_number = sku['articleNumber']
-            variant.price = sku['price']
-            variant.pack_size = sku['friendlyPackSizeTranslation']
-            variant.pattern = sku.dig('pattern', 0, 'name')
-            variant.ean = sku['eanCode']
-            variant.valid = sku['validEcomData']
-            variant.on_sale = sku['readyForSale']
-            variant.ready_mix = !sku['tintedOrReadyMix'].eql?('Tinted')
-            variants << variant
+            variants << new_variant(sku)
           else
             sku['colorIds'].each do |color|
-              variant = SantaMaria::Domain::Variant.new
-              variant.article_number = sku['articleNumber']
-              variant.price = sku['price']
+              variant = new_variant(sku)
 
               variant.name = color.dig('colorCollectionColors', 0, 'colorTranslation')
               variant.color_id = color.dig('colorCollectionColors', 0, 'colorCollectionColorID')
-
-              variant.pack_size = sku['friendlyPackSizeTranslation']
-              variant.pattern = sku.dig('pattern', 0, 'name')
-              variant.ean = sku['eanCode']
-              variant.valid = sku['validEcomData']
-              variant.on_sale = sku['readyForSale']
-              variant.ready_mix = !sku['tintedOrReadyMix'].eql?('Tinted')
 
               variants << variant
             end
@@ -65,6 +39,32 @@ module SantaMaria
         end
 
         variants
+      end
+
+      private
+
+      def new_product(product_data)
+        product = SantaMaria::Domain::Product.new(self)
+        product.global_id = product_data['globalId']
+        product.type = product_data['productType']
+        product.name = product_data['name']
+        product.uri_name = product_data['uri']
+        product.description = product_data['localSlogan']
+        product.image_url = product_data.dig('packshots', 0, 'm')
+        product
+      end
+
+      def new_variant(sku)
+        variant = SantaMaria::Domain::Variant.new
+        variant.article_number = sku['articleNumber']
+        variant.price = sku['price']
+        variant.pack_size = sku['friendlyPackSizeTranslation']
+        variant.pattern = sku.dig('pattern', 0, 'name')
+        variant.ean = sku['eanCode']
+        variant.valid = sku['validEcomData']
+        variant.on_sale = sku['readyForSale']
+        variant.ready_mix = !sku['tintedOrReadyMix'].eql?('Tinted')
+        variant
       end
     end
   end
